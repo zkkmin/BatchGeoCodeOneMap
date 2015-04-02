4e9deb4fb0e801e863ff156d22deb239e1b945b8
zkkmin <zkkmin@gmail.com>
2015-04-02 13:57:57 +0800

    Added New Feature XY To Postal

--- Rest Client/Rest Client.Designer.cs
+++ Rest Client/Rest Client.Designer.cs
@@ -39,6 +39,8 @@
             this.responseText = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioPostalToXY = new System.Windows.Forms.RadioButton();
+            this.radioXYToPostal = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // CallRest
@@ -53,7 +55,7 @@
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(12, 256);
+            this.progressBar.Location = new System.Drawing.Point(12, 311);
             this.progressBar.Name = "progressBar";
             this.progressBar.Size = new System.Drawing.Size(536, 23);
             this.progressBar.TabIndex = 3;
@@ -69,21 +71,21 @@
             // 
             // textBoxInput
             // 
-            this.textBoxInput.Location = new System.Drawing.Point(12, 41);
+            this.textBoxInput.Location = new System.Drawing.Point(12, 17);
             this.textBoxInput.Name = "textBoxInput";
             this.textBoxInput.Size = new System.Drawing.Size(455, 20);
             this.textBoxInput.TabIndex = 4;
             // 
             // textBoxOutput
             // 
-            this.textBoxOutput.Location = new System.Drawing.Point(12, 78);
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 54);
             this.textBoxOutput.Name = "textBoxOutput";
             this.textBoxOutput.Size = new System.Drawing.Size(455, 20);
             this.textBoxOutput.TabIndex = 5;
             // 
             // buttonInput
             // 
-            this.buttonInput.Location = new System.Drawing.Point(473, 39);
+            this.buttonInput.Location = new System.Drawing.Point(473, 15);
             this.buttonInput.Name = "buttonInput";
             this.buttonInput.Size = new System.Drawing.Size(75, 23);
             this.buttonInput.TabIndex = 6;
@@ -93,7 +95,7 @@
             // 
             // buttonOutput
             // 
-            this.buttonOutput.Location = new System.Drawing.Point(473, 75);
+            this.buttonOutput.Location = new System.Drawing.Point(473, 51);
             this.buttonOutput.Name = "buttonOutput";
             this.buttonOutput.Size = new System.Drawing.Size(75, 23);
             this.buttonOutput.TabIndex = 7;
@@ -103,7 +105,7 @@
             // 
             // responseText
             // 
-            this.responseText.Location = new System.Drawing.Point(12, 160);
+            this.responseText.Location = new System.Drawing.Point(12, 215);
             this.responseText.Multiline = true;
             this.responseText.Name = "responseText";
             this.responseText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -114,10 +116,11 @@
             // 
             this.label1.BackColor = System.Drawing.SystemColors.Control;
             this.label1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(19, 167);
             this.label1.Margin = new System.Windows.Forms.Padding(10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(392, 22);
+            this.label1.Size = new System.Drawing.Size(529, 22);
             this.label1.TabIndex = 8;
             this.label1.Text = "*Input file must have a column of Singapore postal codes";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -126,20 +129,46 @@
             // 
             this.textBox1.BackColor = System.Drawing.Color.White;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(563, 41);
+            this.textBox1.Location = new System.Drawing.Point(563, 17);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(203, 231);
+            this.textBox1.Size = new System.Drawing.Size(203, 317);
             this.textBox1.TabIndex = 9;
             this.textBox1.Text = "1) Click Input File Button and select the input file\r\n\r\n2) Click Output File Butt" +
     "on and select the output file\r\n\r\n4) Click Run and wait for the result to be proc" +
     "essed\r\n\r\n\r\n";
             // 
+            // radioPostalToXY
+            // 
+            this.radioPostalToXY.AutoSize = true;
+            this.radioPostalToXY.Checked = true;
+            this.radioPostalToXY.Location = new System.Drawing.Point(12, 86);
+            this.radioPostalToXY.Name = "radioPostalToXY";
+            this.radioPostalToXY.Size = new System.Drawing.Size(83, 17);
+            this.radioPostalToXY.TabIndex = 10;
+            this.radioPostalToXY.TabStop = true;
+            this.radioPostalToXY.Text = "Postal -> XY";
+            this.radioPostalToXY.UseVisualStyleBackColor = true;
+            this.radioPostalToXY.CheckedChanged += new System.EventHandler(this.radioPostalToXY_CheckedChanged);
+            // 
+            // radioXYToPostal
+            // 
+            this.radioXYToPostal.AutoSize = true;
+            this.radioXYToPostal.Location = new System.Drawing.Point(12, 121);
+            this.radioXYToPostal.Name = "radioXYToPostal";
+            this.radioXYToPostal.Size = new System.Drawing.Size(83, 17);
+            this.radioXYToPostal.TabIndex = 11;
+            this.radioXYToPostal.Text = "XY -> Postal";
+            this.radioXYToPostal.UseVisualStyleBackColor = true;
+            this.radioXYToPostal.CheckedChanged += new System.EventHandler(this.radioXYToPostal_CheckedChanged);
+            // 
             // RestClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 284);
+            this.ClientSize = new System.Drawing.Size(778, 339);
+            this.Controls.Add(this.radioXYToPostal);
+            this.Controls.Add(this.radioPostalToXY);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonOutput);
@@ -169,6 +198,8 @@
         private System.Windows.Forms.TextBox responseText;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioPostalToXY;
+        private System.Windows.Forms.RadioButton radioXYToPostal;
     }
 }
 
--- Rest Client/Rest Client.cs
+++ Rest Client/Rest Client.cs
@@ -26,8 +26,12 @@ namespace Rest_Client
         private List<string> lines = new List<string>();
         private List<string> resultLines = new List<string>();
         private string outputFile = "output.csv";
+        
         private const string fileFilter = "Text Files (*.txt) |*.txt|Comma Separated Value (*.csv)|*.csv";
-
+        private const string urlGeocode = "http://www.onemap.sg/API/services.svc/basicSearch?token=qo/s2TnSUmfLz+32CvLC4RMVkzEFYjxqyti1KhByvEacEdMWBpCuSSQ+IFRT84QjGPBCuz/cBom8PfSm3GjEsGc8PkdEEOEr&searchVal=";
+        private const string optionsGeocode = "&otptFlds=SEARCHVAL,CATEGORY&returnGeom=1&rset=1";
+        private const string urlReverseGeocode = "http://www.onemap.sg/API/services.svc/revgeocode?token=qo/s2TnSUmfLz+32CvLC4RMVkzEFYjxqyti1KhByvEacEdMWBpCuSSQ+IFRT84QjGPBCuz/cBom8PfSm3GjEsGc8PkdEEOEr&location="; //37949.0265,34465.7386
+        private const string optionsReverseGeocode = "&buffer=1&addressType=HDB";
         public RestClient()
         {
             InitializeComponent();
@@ -38,9 +42,36 @@ namespace Rest_Client
             callRest();
         }
 
+        // parse result for Reversed Geocoding XY to address/postal
+
+        private Tuple<string, string, string, string, string> parseReversedGeocodeResult(string x, string y, string result)
+        {
+            const string errorMessage = "ErrorMessage";
+
+            if (result.Contains(errorMessage))
+            {
+                return Tuple.Create<string, string, string, string, string>(x, y, "NotFound", "NotFound", "NotFound");
+            }
+            else
+            {
+                JObject jobjResult = JObject.Parse(result);
+                //{ "GeocodeInfo":[{"BUILDINGNAME":"PING YI GARDENS","BLOCK":"1","ROAD":"CHAI CHEE ROAD","POSTALCODE":"461001","XCOORD":"37949.02647326","YCOORD":"34465.73856911"}]}
+                IList<JToken> searchResults = jobjResult["GeocodeInfo"].Children().ToList();
+
+                PostalResult postalResult = JsonConvert.DeserializeObject<PostalResult>(searchResults[0].ToString());
+
+                return Tuple.Create<string, string, string, string, string>(x, y, postalResult.block, postalResult.road, postalResult.postalCode);
+                
+            }
+        }
+
+
+
+
+        // parsing result for Geocoding postal code to XY
         private Tuple<string, string, string> parseResult(string postal ,string result)
         {
-            string errorMessage = "ErrorMessage";
+            const string errorMessage = "ErrorMessage";
             
 
             if (result.Contains(errorMessage))
@@ -78,31 +109,56 @@ namespace Rest_Client
 
             try
             {
-                string url = "http://www.onemap.sg/API/services.svc/basicSearch?token=qo/s2TnSUmfLz+32CvLC4RMVkzEFYjxqyti1KhByvEacEdMWBpCuSSQ+IFRT84QjGPBCuz/cBom8PfSm3GjEsGc8PkdEEOEr&searchVal=";
-                string options = "&otptFlds=SEARCHVAL,CATEGORY&returnGeom=1&rset=1";
                
                 progressBar.Maximum =lines.Count;
                 responseText.Text += "\r\nGetting data ...";
-                resultLines.Add("Postal,X,Y");
-                int i = 0;
-                foreach (string postal in lines)
-	            {
-                    
-                    string completeUrl = url + postal + options;
-                    HttpResponseMessage response = await client.GetAsync(completeUrl);
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
 
-                    Tuple<string, string, string> restultTuple = parseResult(postal, responseBody);
 
-                    resultLines.Add(restultTuple.Item1 + "," + restultTuple.Item2 + "," + restultTuple.Item3);
-                    i++;
-                    progressBar.Value = i;
-                    
-                    progressBar.Update();
-	            }
+                if (radioPostalToXY.Checked == true) // postal to XY is geocoding
+                {
+                    resultLines.Add("Postal,X,Y");
+                    int i = 0;
 
-          
+                    foreach (string postal in lines)
+                    {
+
+                        string completeUrl = urlGeocode + postal + optionsGeocode;
+                        HttpResponseMessage response = await client.GetAsync(completeUrl);
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+
+                        Tuple<string, string, string> restultTuple = parseResult(postal, responseBody);
+
+                        resultLines.Add(restultTuple.Item1 + "," + restultTuple.Item2 + "," + restultTuple.Item3);
+                        i++;
+                        progressBar.Value = i;
+
+                        progressBar.Update();
+                    }
+                }
+                else  // XY to Postal is reversed geocoding
+                {
+                    resultLines.Add("X,Y,Block,Street,Postal");
+                    int j = 0;
+
+                    foreach (string xy in lines)
+                    {
+                        string x = xy.Split(',')[0];
+                        string y = xy.Split(',')[1];
+
+                        string completeUrl = urlReverseGeocode + xy + optionsReverseGeocode;
+                        HttpResponseMessage response = await client.GetAsync(completeUrl);
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+
+                        Tuple<string, string, string, string, string> resultTuple = parseReversedGeocodeResult(x, y, responseBody);
+
+                        resultLines.Add(resultTuple.Item1 + "," + resultTuple.Item2 + "," + resultTuple.Item3 + "," + resultTuple.Item4 + "," + resultTuple.Item5);
+                        j++;
+                        progressBar.Value = j;
+                        progressBar.Update();
+                    }
+                }
                 
             }
             catch (HttpRequestException e)
@@ -171,12 +227,58 @@ namespace Rest_Client
 
         }
 
+        private void radioPostalToXY_CheckedChanged(object sender, EventArgs e)
+        {
+            displayInstruction();
+        }
+
+        private void radioXYToPostal_CheckedChanged(object sender, EventArgs e)
+        {
+            displayInstruction();
+        }
+
+        private void displayInstruction()
+        {
+            if (radioPostalToXY.Checked)
+            {
+                label1.Text = "*Input file must have a column of Singapore postal codes";
+            }
+            else
+            {
+                label1.Text = "*Input file must have a column of XY values separated by comma";
+            }
+        }
+
+        
+
 
 
        
     }
 
+    [DataContract]
+    public class PostalResult
+    {
+        // { "GeocodeInfo":[{"BUILDINGNAME":"PING YI GARDENS","BLOCK":"1","ROAD":"CHAI CHEE ROAD","POSTALCODE":"461001","XCOORD":"37949.02647326","YCOORD":"34465.73856911"}]}
+        [DataMember(Name = "BUILDINGNAME")]
+        public string buildingName { get; set; }
 
+        [DataMember(Name = "BLOCK")]
+        public string block { get; set; }
+
+        [DataMember(Name = "ROAD")]
+        public string road { get; set; }
+
+        [DataMember(Name = "POSTALCODE")]
+        public string postalCode { get; set; }
+
+        [DataMember(Name = "XCOORD")]
+        public string xcoord { get; set; }
+
+        [DataMember(Name = "YCOORD")]
+        public string ycoord { get; set; }
+
+    }
 
     [DataContract]
     public class XYResult
